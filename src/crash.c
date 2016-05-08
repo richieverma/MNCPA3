52877cd1466054a286e557245b07156af4977801
Richie Verma <richieverma@gmail.com>
2016-05-08 19:14:26 -0400

    Implemented crash_response. Need to check if this will have implications on other functions. Should this router be removed from other lists like router_list or neighbour_list or track_update_list

--- src/crash.c
+++ src/crash.c
@@ -27,6 +27,23 @@
 #include "../include/control_header_lib.h"
 #include "../include/network_util.h"
 
+//CONTROL CODE 4
 void crash_response(int sock_index)
 {
+    //Send Control response for CRASH with no payload
+	uint16_t response_len;
+	char *cntrl_response_header, *cntrl_response;
+
+	cntrl_response_header = create_response_header(sock_index, 4, 0, 0);
+
+	response_len = CNTRL_RESP_HEADER_SIZE;
+	cntrl_response = (char *) malloc(response_len);
+	/* Copy Header */
+	memcpy(cntrl_response, cntrl_response_header, CNTRL_RESP_HEADER_SIZE);
+	free(cntrl_response_header);
+
+	sendALL(sock_index, cntrl_response, response_len);
+
+	free(cntrl_response);
+	exit(0);
 }
